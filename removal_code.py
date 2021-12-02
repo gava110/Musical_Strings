1a8102f6ffd354413e8279f066e4cf1520512d07
gava110 <filippo.gavagnin@student.h-farm.com>
2021-12-02 20:53:03 +0100

    Added automated lyrics language recognition

--- removal_code.py
+++ removal_code.py
@@ -1,15 +1,22 @@
-#Import module for stopwords removal 
+#Import modules for stopwords removal
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
+#Import modules for automated language recognition 
+from langdetect import detect
+import iso639
 
-def stopremoval (song, language):
-#Define language specific stopwords to remove from retrieved lyrics
-    stop_words = set(stopwords.words(language)
+def stopremoval (song):
 
+    #Detect lyrics' language and convert language ISO639 code into plain text 
+    language = detect(song)
+    lang=iso639.to_name(language)
+    
+    #Define language specific stopwords to be removed from retrieved lyrics
+    stop_words = set(stopwords.words(lang))
 
-    ​#Turn all words inside lyrics in lowercase and split them into tokens
+    #Turn all words inside lyrics in lowercase and split them into tokens
     song=song.lower()
     word_tokens = word_tokenize(song)
 
@@ -22,4 +29,4 @@ def stopremoval (song, language):
             filtered_song.append(words)
         
     return filtered_song
-        
+    
