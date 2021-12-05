80dd8d2108e2686fd511a4637b244b4034edc3c8
Sarojdeep99 <94049769+Sarojdeep99@users.noreply.github.com>
2021-12-05 15:58:45 +0100

    Update Stemming and Lemmatization 2.py

--- Stemming and Lemmatization 2.py
+++ Stemming and Lemmatization 2.py
@@ -3,62 +3,29 @@
 
 # ## Stemming 
 
-# In[40]:
-
-
 #Getting the info about the song chosen
 get_song= (get_lyric(artist1, song1))
 #print(canzone.split())
-
-
-# In[42]:
-
 
 from textblob import TextBlob
 #Applying the stemming function 
 song_stem = sorted(set(TextBlob(get_song).lower().words.stem()))
 #song_stem
 
-
-# In[43]:
-
-
 get_song2= (get_lyric(artist2, song2))
 #print(canzone.split())
-
-
-# In[46]:
-
 
 song_stem2 = sorted(set(TextBlob(get_song2).lower().words.stem()))
 #song_stem2
 
 
-# In[47]:
-
-
-#Add Jaccard Similarity
-
-
 # ## Lemmatization
-
-# In[50]:
-
 
 #Applying the lemmatisation function
 song_lemm = sorted(set(TextBlob(get_song).lower().words.lemmatize()))
 #song_lemm
 
-
-# In[53]:
-
-
 song_lemm2 = sorted(set(TextBlob(get_song2).lower().words.lemmatize()))
 #song_lemm2
 
-
-# In[ ]:
-
-
-
-
+#Add Jaccard Similarity here 
