964ef8efbf3a365f81f4a95bd3006a0f89423d7c
Pit2k <pietro.dalfabbro@student.h-farm.com>
2021-12-06 11:27:04 +0100

    adding intersection code

---
+++ intersection_code.py
@@ -0,0 +1,58 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 10,
+   "id": "b2fcfd66",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def intersection(list1, list2):\n",
+    "    inter1 = set(list1)\n",
+    "    inter2 = set(list2)\n",
+    "    res = float(len(inter1.intersection(inter2)))\n",
+    "    return res\n",
+    "\n",
+    "\n",
+    "    \n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "9baf2a95",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "4a6d413f",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
