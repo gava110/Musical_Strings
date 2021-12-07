f1a35c00e93f5a2d68e3a1831e88739233f7e7c8
gava110 <filippo.gavagnin@student.h-farm.com>
2021-12-07 17:24:17 +0100

    Improved stopwords filtering

--- removal_code.py
+++ removal_code.py
@@ -1,7 +1,8 @@
-#Import modules for stopwords removal
+#Import modules for lyrics tokenization and stopwords removal
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+import string
 
 #Import modules for automated language recognition 
 from langdetect import detect
@@ -13,8 +14,9 @@ def stopremoval (song):
     language = detect(song)
     lang=iso639.to_name(language)
     
-    #Define language specific stopwords to be removed from retrieved lyrics
-    stop_words = set(stopwords.words(lang))
+    #Define language specific stopwords + extra ones to be removed from retrieved lyrics
+    extra_stopwords = ["'m", "'s", "'re", "n't", "'ve", "'d", "'ll", "oh", "ah", "eh", "uh"]
+    stop_words = set((stopwords.words(lang))+list(string.punctuation)+extra_stopwords)
 
     #Turn all words inside lyrics in lowercase and split them into tokens
     song=song.lower()
