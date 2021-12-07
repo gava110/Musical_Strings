9a8257873762413c261a572c9f7957d9e7736864
Riccardo Ceolin <riccardoceolin@MacBook-Air-5.local>
2021-12-07 13:00:23 +0100

    deleting old <stemming and lemmatisation> code and rename teh new one

--- stemming and lemmatization.py
+++ stemming and lemmatization.py
@@ -1,31 +1,37 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# ## Stemming 
-
-#Getting the info about the song chosen
-get_song= (get_lyric(artist1, song1))
-#print(canzone.split())
-
-from textblob import TextBlob
-#Applying the stemming function 
-song_stem = sorted(set(TextBlob(get_song).lower().words.stem()))
-#song_stem
-
-get_song2= (get_lyric(artist2, song2))
-#print(canzone.split())
-
-song_stem2 = sorted(set(TextBlob(get_song2).lower().words.stem()))
-#song_stem2
+# In[57]:
 
 
-# ## Lemmatization
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+  
+def stemming (songs):
+    
+    ps = PorterStemmer()
+    
+    #creating a list for stemmed words
+    stemmed_songs = []
+    
+    #applying the algorithm
+    for w in songs:
+        stemmed_songs.append(ps.stem(w))
+    return stemmed_songs
 
-#Applying the lemmatisation function
-song_lemm = sorted(set(TextBlob(get_song).lower().words.lemmatize()))
-#song_lemm
 
-song_lemm2 = sorted(set(TextBlob(get_song2).lower().words.lemmatize()))
-#song_lemm2
+# In[59]:
 
-#Add Jaccard Similarity here 
+
+from nltk.stem import WordNetLemmatizer
+
+def lemmatization (songs):
+    
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_songs = []
+    
+    for w in songs:
+        lemmatized_songs.append(lemmatizer.lemmatize(w))
+    
+    return lemmatized_songs
+
