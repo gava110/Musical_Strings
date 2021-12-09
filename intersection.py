74febd2a0f65d050b6ed093ee2a1c8ca314b9b68
Riccardo Ceolin <riccardoceolin@MacBook-Air-5.local>
2021-12-09 18:36:43 +0100

    bug fixed

--- intersection.py
+++ intersection.py
@@ -1,7 +1,8 @@
+import copy 
 def intersection(list1, list2):
     inter1 = set(list1)
     inter2 = set(list2)
-    res = float(len(inter1.intersection(inter2)))
+    res = len((copy.deepcopy(inter1)).intersection(inter2))
     return res
 
 
