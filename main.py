a197ae01f5f8c9f68bbe1a75ab1e28e294474d94
Riccardo Ceolin <riccardoceolin@MacBook-Air-5.local>
2021-12-13 18:51:47 +0100

    pep8 and bug fixing to main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 The aim of the project is to performe the similarity between two of the best 500 songs in history.
 User should enter one pair of artist and song, and eventually make explicit the similarity method to peform.
-This is the main code of the software. In this code we combine all function within different modules 
+This is the main code of the software. In this code we combine all function within different modules
 in order to performe the software aim.
 
 """
@@ -25,7 +25,7 @@ import copy
 
 """
 This part of the main code, get the informations from the user through argparse method.
-Two songs and relative artists are positional string arguments, in other words, without 
+Two songs and relative artists are positional string arguments, in other words, without
 these two entries, the software does not work.
 Preference (abbreviate with -p) is optional argument required to the user. It could be
 "jaccard" or "intersection", if is not specified, defoult value is "jaccard".
@@ -34,39 +34,45 @@ Preference (abbreviate with -p) is optional argument required to the user. It co
 
 
 parser = ap.ArgumentParser()
-parser.add_argument("artist1", help="First artist", type= str)
-parser.add_argument("song1", help="Song of the first artist", type= str)
-parser.add_argument("artist2", help="Second artist", type= str)
-parser.add_argument("song2", help="Song of the Second artist", type= str)
-parser.add_argument("-p", "--preference", help="Enter similarity method preference",
-					default="jaccard", choices= ["jaccard","intersection"], type=str)
+parser.add_argument("artist1", help="First artist", type=str)
+parser.add_argument("song1", help="Song of the first artist", type=str)
+parser.add_argument("artist2", help="Second artist", type=str)
+parser.add_argument("song2", help="Song of the Second artist", type=str)
+parser.add_argument(
+    "-p",
+    "--preference",
+    help="Enter similarity method preference",
+    default="jaccard",
+    choices=[
+        "jaccard",
+        "intersection"],
+    type=str)
 args = parser.parse_args()
 
-#rename user entries
-artist1=args.artist1
-song1=args.song1
-artist2=args.artist2
-song2=args.song2
-preference= args.preference
+# rename user entries
+artist1 = args.artist1
+song1 = args.song1
+artist2 = args.artist2
+song2 = args.song2
+preference = args.preference
 
 
 """
 Try and expect method allows to return "not found" statement in case of "KeyError".
-If the one of the song is not found by the API (inside lyrics module), code retun "KeyError" 
+If the one of the song is not found by the API (inside lyrics module), code retun "KeyError"
 and statement is printed. If the process does not produce errors, it store the two lyrics.
 
 """
 
 
 try:
-	text1= get_lyric(artist1, song1)
+    text1 = get_lyric(artist1, song1)
 except KeyError:
-	print ("{} - {} not found".format(artist1,song1))
-try :
-	text2= get_lyric(artist2, song2)
+    print("{} - {} not found".format(artist1, song1))
+try:
+    text2 = get_lyric(artist2, song2)
 except KeyError:
-	print ("{} - {} not found".format(artist2,song2))
-
+    print("{} - {} not found".format(artist2, song2))
 """
 We upload "Top 500 Songs.csv" to have list of best 500 songs in history.
 Then all columns, exept for "artist" and "title", are dropped as the code does
@@ -75,51 +81,48 @@ not use them.
 """
 
 
-top500 = pandas.read_csv("Top 500 Songs.csv", encoding= 'Latin 1')
+top500 = pandas.read_csv("Top 500 Songs.csv", encoding='Latin 1')
 top500 = top500[["artist", "title"]]
 
 
-#Check if the two songs are in top 500 dataset through "check" module.
-if artitle_check(artist1,song1,top500) == False:
-	print ("Your FIST artist is not on Top 500 songs")
-if artitle_check(artist2,song2,top500) == False:
-	print ("Your SECOND artist is not on Top 500 songs")
+# Check if the two songs are in top 500 dataset through "check" module.
+if artitle_check(artist1, song1, top500) == False:
+    print("Your FIST artist is not on Top 500 songs")
+    exit()
+if artitle_check(artist2, song2, top500) == False:
+    print("Your SECOND artist is not on Top 500 songs")
+    exit()
 
 
-#Remove stopwords and punctuation through "stopwords" module.
+# Remove stopwords and punctuation through "stopwords" module.
 filtered1 = stopremoval(text1)
 filtered2 = stopremoval(text2)
 
 
-#Store lemmatised list of strings
+# Store lemmatised list of strings
 lem1 = lemmatization(filtered1)
 lem2 = lemmatization(filtered2)
 
 
+if preference == "jaccard":
 
-if preference== "jaccard":
+    # Perform jaccard similarity through "jaccard" module.
+    similarity_l = jsimilarity(lem1, lem2)
 
-	#Perform jaccard similarity through "jaccard" module.
-	similarity_l= jsimilarity(lem1,lem2)
-
-	print ("{} by {} and {} by {} are similar  according to lemmatization by {}%".format(song1,artist1,song2,artist2 ,round( similarity_l*100,2)))
-
-
-if preference=="intersection":
-
-	#Intersection of set thorough "intersection module"
-	inter_l= intersection(lem1, lem2)
-	print ( "Number of intersection by lemmatization is {}".format(inter_l) )
-
-
-
+    print(
+        "{} by {} and {} by {} are similar  according to lemmatization by {}%".format(
+            song1,
+            artist1,
+            song2,
+            artist2,
+            round(
+                similarity_l *
+                100,
+                2)))
 
 
+if preference == "intersection":
 
-
-
-
-
-
-
-
+    # Intersection of set thorough "intersection module"
+    inter_l = intersection(lem1, lem2)
+    print("Number of intersection by lemmatization is {}".format(inter_l))
