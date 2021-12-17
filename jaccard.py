94f82c14eecf2b387b0634b8419067828e3d8680
Pit2k <pietro.dalfabbro@student.h-farm.com>
2021-12-17 16:16:52 +0100

    Fixed unit testing on jaccard similarity, fixed pep8 on jaccard.py , intersection.py and test_jaccard.py

--- jaccard.py
+++ jaccard.py
@@ -1,9 +1,26 @@
 import copy
+"""
+This function called jsimilarity will take two lists
+as input from previous code, containing the lyrics of
+the two songs chosen by the user,
+and return the jaccard similarity value
+between the two songs.
+"""
+
+
 def jsimilarity(list1, list2):
+
     jacc1 = set(list1)
     jacc2 = set(list2)
-    inter= (copy.deepcopy(jacc1)).intersection(jacc2)
-    union= jacc1.union(jacc2)
-    res = float (len(inter)/len (union))
-    return res
 
+    # intersection between the two songs 
+    inter = (copy.deepcopy(jacc1)).intersection(jacc2)
+    
+    # union between the two songs
+    union = jacc1.union(jacc2)
+    
+    # jaccard similarity formula
+    res = float(len(inter) / len(union))
+    
+    # jaccard similarity value
+    return res
