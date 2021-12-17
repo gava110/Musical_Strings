972eae3bbe1206f2994b7eee64ec17422e2e1c88
Sarojdeep99 <94049769+Sarojdeep99@users.noreply.github.com>
2021-12-17 23:15:17 +0100

    Update test_stopwords.py

--- Tests/test_stopwords.py
+++ Tests/test_stopwords.py
@@ -1,5 +1,5 @@
 import unittest
-import stopwords_del
+import stopwords
 
 """
 
@@ -19,8 +19,9 @@ class TestStopwords(unittest.TestCase):
     def test_stopwords_success(self):
         
         raw="Don't wanna be an american idiot, Don't want a nation under the new media, And can you hear the sound of hysteria the subliminal mind fuck America"
-        output=stopwords_del.stopremoval(raw)
+        output=stopwords.stopremoval(raw)
         self.assertTrue(type(output)==list)
 
-    if __name__ == '__main__':
+     if __name__ == '__main__':
         unittest.main(argv=['first-arg-is-ignored'], exit=False) 
+        
