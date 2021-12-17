329da099d28b03400cffedcbc4e25d5e45764aea
Riccardo Ceolin <riccardoceolin@MacBook-Air-5.local>
2021-12-17 21:48:56 +0100

    running stopwords test and check system functioning

--- check.py
+++ check.py
@@ -1,25 +1,19 @@
-import pandas
-
-#Create a pandas dataframe from  Top 500 Songs.csv and select just artist and song title
-top500 = pandas.read_csv("Top 500 Songs.csv", encoding= 'Latin 1')
-top500 = top500[["artist", "title"]]
-
-#Turn all vlaues in the newly created top500 dataframe lowercase
-for i in range (len(top500)):
-    top500["artist"][i]=top500["artist"][i].lower()
-    top500["title"][i]=top500["title"][i].lower()
-
-def artitle_check(artist, title):
+def artitle_check(artist, title,file):
     #Makes sure any input from user is turned lowercase
+    #Turn all vlaues in the newly created file dataframe lowercase
+    for i in range (len(file)):
+        file["artist"][i]=file["artist"][i].lower()
+        file["title"][i]=file["title"][i].lower()
+
     artist= str(artist).lower()
     title=str(title).lower()
     
-    #Search for inputted artist and return True if it is inside top500
-    if artist in top500["artist"].tolist():
+    #Search for inputted artist and return True if it is inside file
+    if artist in file["artist"].tolist():
         return True
     return False
     
-    #Search for inputted song title and return True if it is inside top500
-    if title in top500["title"].tolist():
+    #Search for inputted song title and return True if it is inside file
+    if title in file["title"].tolist():
         return True
     return False
